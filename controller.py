cbab43d65142313803b51f0820589cc4d1c29a88
Natalia Kushnir <natalka7618@yandex.ru>
2022-08-04 22:52:07 +0300

    удален init

--- controller.py
+++ controller.py
@@ -6,8 +6,7 @@ def button_click():
     surname = view.set_surname()
     phone = view.set_phone()
     description = view.set_description()
-    
-    data.init(name, surname, phone, description)
+
     data.add_member_txt(name, surname, phone, description)
-    view.view_data(name, surname, phone, description)
     data.add_member_csv(name, surname, phone, description)
+    #view.view_data(name, surname, phone, description)
--- data.py
+++ data.py
@@ -1,17 +1,5 @@
 import csv
 
-name = ''
-surname = ''
-phone_number = ''
-description = ''
-
-def init (name, surname, phone_number, description):
-    name = name
-    surname = surname
-    phone_number = phone_number
-    description = description
-
-
 def add_member_txt(name, surname, phone_number, description):
     f = open('Phone_book.txt', 'a', encoding ='utf-8')
     f.write(f'Контакт: {name} {surname} {phone_number} - {description}')
@@ -20,13 +8,7 @@ def add_member_txt(name, surname, phone_number, description):
     f.close()   
         
 def add_member_csv(name, surname, phone_number, description):
-
+    data = [(name, surname, phone_number, description)]
     with open("Phone_book.csv", "a", encoding ='utf-8') as fp:
         writer = csv.writer(fp, delimiter='|')
-
-        data = [
-            (name, surname, phone_number, description)
-        ]
-
         writer.writerows(data)
-
